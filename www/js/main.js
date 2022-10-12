a95b3393de2a700ba3b4294fa1406b73dc272ac8
bigreni <santhanakrishnan.sai@gmail.com>
2022-10-12 13:08:17 -0500

    Ready to roll

--- www/js/main.js
+++ www/js/main.js
@@ -138,48 +138,8 @@ function getStopList(json) {
 }
 
 function displayError(error) {
-    reset(true);
-    displayResultsBox($("#errorTemplate").render({ error: error }));
+    $("#message").text(error);   
 }
-
-function displayResultsBox(html) {
-    // Unfortunately IE9 leaves	artifacts
-    var radius = $("#contentBox").css("border-radius");
-
-    $(html).hide().appendTo("#contentBox").toggle(500, function () {
-        $("#contentBox").css("border-radius", radius);
-        $(this).animate({ opacity: "1" }, 200);
-    });
-}
-
-function reset(instantRemove) {
-    if (timer != null) {
-        window.clearInterval(timer);
-        timer = null;
-    }
-
-    if ($("#resultBox").length > 0) {
-        if (instantRemove)
-            $("#resultBox").remove();
-        else
-            removeResultBox();
-    }
-}
-
-function removeResultBox() {
-    // Unfortunately IE9 leaves	artifacts
-    var shadow = $("#contentBox").css("box-shadow");
-    var shadowHide = shadow;
-
-    $("#resultBox").animate({ opacity: "0" }, 200, function () {
-        $("#contentBox").css("box-shadow", shadowHide);
-        $(this).toggle(500, function () {
-            $("#contentBox").css("box-shadow", shadow);
-            $(this).remove();
-        })
-    });
-}
-
 
 function getDirections() {
     var url = encodeURI("https://webservices.umoiq.com/service/publicJSONFeed?command=routeConfig&a=reno&r=" + $("#MainMobileContent_routeList").val());
@@ -214,21 +174,21 @@ function getDirections() {
 function getStops() {
     var dir = $("#MainMobileContent_directionList").val()
     var list = $("#MainMobileContent_stopList");
+    $("span").remove();
+    $(".dropList").select2();
     $(list).empty();
-    $(list).append($("<option disabled/>").val("0").text("- Select Direction -"));
+    $(list).append($("<option disabled/>").val("0").text("- Select Stop -"));
     var stops = routeConfig.route.direction[dir].stop;
     for (var i in stops) {
         var stopTag = stops[i].tag;
         var stopName = stopList[stopTag];
         $(list).append($("<option />").val(stopTag).text(stopName));
     }
-    $("span").remove();
-    $(".dropList").select2();
+    $(list).val(0);
 }
 
 function getArrivals()
 {
-    alert('1');
     var route = $("#MainMobileContent_routeList").val()
     var stopCode = $("#MainMobileContent_stopList").val();
     getArrivalTimes(route, stopCode);
@@ -238,20 +198,19 @@ function getArrivalTimes(route, stopCode) {
     var query_url = encodeURI("https://webservices.umoiq.com/service/publicJSONFeed?command=predictions&a=reno&r=" + route + "&s=" + stopCode);
     var outputContainer = $('.js-next-bus-results');
     $.getJSON(query_url, function(json) {
-        var arrivalHtml = "";
+        var arrivalHtml = '<table id="tblResults" cellpadding="0" cellspacing="0">';
         var preds = {};
         if (json.Error) {
-            arrivalHtml += '<div class="nb-card no-results><span class="nb-card-title">No Results</span>';
+            arrivalHtml += '<tr><td>No Results</td></tr></table>';
             $(outputContainer).html(arrivalHtml).show();
             return;
         }
         // This element only exists when no results
         if (json.predictions.dirTitleBecauseNoPredictions) {
-            arrivalHtml += '<div class="nb-card no-results><span class="nb-card-title">No Results</span>';
+            arrivalHtml += '<tr><td>No Results</td></tr></table>';
             $(outputContainer).html(arrivalHtml).show();
             return;
         }
-    alert('2');
         var rname = json.predictions.routeTag;
         // Check to see if multiple directions were provided or just one
         if (json.predictions.direction.constructor === Array) {
@@ -262,358 +221,56 @@ function getArrivalTimes(route, stopCode) {
             preds = json.predictions.direction.prediction;
             dir = json.predictions.direction.title;
         }
-        arrivalHtml += '<div class="nb-card"><span class="nb-card-title">' + rname + " " + dir + '</span>';
+        arrivalHtml += '<tr class="header"><th>' + rname + " " + dir + '</th></tr><tr><td class="spacer" colspan="3"></td></tr>'
         if (preds.constructor === Array) {
             for (var i in preds) {
-                arrivalHtml += '<span class="nb-card-time">' + preds[i].minutes + ' min</span>';
+                arrivalHtml += '<tr class="predictions"><td>' + preds[i].minutes + ' min</td></tr>';
+                arrivalHtml += '<tr><td class="spacer" colspan="3"></td></tr>';
             }
         } else {
-            arrivalHtml += '<span class="nb-card-time">' + preds.minutes + ' min</span>';
+            arrivalHtml += '<tr class="predictions"><td>' + preds.minutes + ' min</td></tr>';
         }
-        alert(arrivalHtml);
-        arrivalHtml += '</div>';
+        arrivalHtml += '</table>';
+        document.getElementById('btnSave').style.visibility = "visible";
         $(outputContainer).html(arrivalHtml).show();
     });
 }
 
-function displayError(error) {
-    reset(true);
-    displayResultsBox($("#errorTemplate").render({ error: error }));
-}
-
-function displayResultsBox(html) {
-    // Unfortunately IE9 leaves	artifacts
-    var radius = $("#contentBox").css("border-radius");
-
-    $(html).hide().appendTo("#contentBox").toggle(500, function () {
-        $("#contentBox").css("border-radius", radius);
-        $(this).animate({ opacity: "1" }, 200);
-    });
-}
-
-function reset(instantRemove) {
-    if (timer != null) {
-        window.clearInterval(timer);
-        timer = null;
-    }
-
-    if ($("#resultBox").length > 0) {
-        if (instantRemove)
-            $("#resultBox").remove();
+function saveFavorites()
+{
+    var favStop = localStorage.getItem("Favorites");
+    var newFave = $('#MainMobileContent_routeList option:selected').val() + ">" + $("#MainMobileContent_directionList option:selected").val() + ">" + $("#MainMobileContent_stopList option:selected").val() + ":" + $('#MainMobileContent_routeList option:selected').text() + " > " + $("#MainMobileContent_directionList option:selected").text() + " > " + $("#MainMobileContent_stopList option:selected").text();
+        if (favStop == null)
+        {
+            favStop = newFave;
+        }   
+        else if(favStop.indexOf(newFave) == -1)
+        {
+            favStop = favStop + "|" + newFave;               
+        }
         else
-            removeResultBox();
-    }
+        {
+            $("#message").text('Stop is already favorited!!');
+            return;
+        }
+        localStorage.setItem("Favorites", favStop);
+        $("#message").text('Stop added to favorites!!');
 }
 
-function removeResultBox() {
-    // Unfortunately IE9 leaves	artifacts
-    var shadow = $("#contentBox").css("box-shadow");
-    var shadowHide = shadow;
+function loadFaves()
+{
+showAd();
+window.location = "Favorites.html";
+}
 
-    $("#resultBox").animate({ opacity: "0" }, 200, function () {
-        $("#contentBox").css("box-shadow", shadowHide);
-        $(this).toggle(500, function () {
-            $("#contentBox").css("box-shadow", shadow);
-            $(this).remove();
-        })
+function showAd()
+{
+document.getElementById("screen").style.display = 'block';     
+if ((/(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent))) {
+    AdMob.isInterstitialReady(function(isready){
+        if(isready) 
+            AdMob.showInterstitial();
     });
 }
-
-// var	TransitMaster =	TransitMaster || {};
-
-// TransitMaster.StopTimes = function (options) {
-//     var settings = { arrivals: null, headingLabel: null, includeStops: true };
-//     $.extend(settings, options);
-
-//     var timer = null;
-//     var initialView = true;
-//     initialize();
-
-//     function initialize() {
-//         $("#MainMobileContent_routeList").bind("change", function () {
-//             var temp = $("#MainContent_routeList").val();
-
-//             if (temp != "") {
-//                 $.cookie("route", temp, { expires: 30 });
-//                 getDirections();
-//             }
-//         });
-
-//         $("#MainMobileContent_directionList").bind("change", function () {
-//             var temp = $("#MainContent_directionList").val();
-
-//             if (temp != "") {
-//                 $.cookie("direction", temp, { expires: 30 });
-//                 reset();
-
-//                 if (settings.includeStops)
-//                     getStops();
-//             }
-//         });
-
-//         if (settings.includeStops) {
-//             $("#MainMobileContent_stopList").bind("change", function () {
-//                 var temp = $("#MainMobileContent_stopList").val();
-
-//                 if (temp != "") {
-//                     $.cookie("stop", temp, { expires: 30 });
-//                     getArrivalTimes();
-//                 }
-//             });
-//         }
-
-//         getRoutes();
-//     }
-
-
-//     function checkListCookie(key, list) {
-//         if (initialView) {
-//             var temp = $.cookie(key);
-//             if (temp != null && $("#" + list + " option[value=" + temp + "]").length > 0) {
-//                 $("#" + list).val(temp).change();
-//                 return true;
-//             }
-//             else
-//                 initialView = false;
-//         }
-
-//         return false;
-//     }
-
-//     function getRoutes() {
-//         //$("#MainMobileContent_routeList").text("Loading	routes...");
-//         $("#routeWait").removeClass("hidden");
-
-//         $.ajax({
-//             type: "POST",
-//             url: "http://webwatch.rtcwashoe.com/TMwebwatch/Arrivals.aspx/getRoutes",
-//             contentType: "application/json;	charset=utf-8",
-//             dataType: "json",
-//             success: function (msg) {
-//                 if (msg.d == null || msg.d.length == 0) {
-//                     $("#MainMobileContent_routeList").text("No routes found");
-//                     return;
-//                 }
-
-//                 var list = $("#MainMobileContent_routeList");
-
-//                 $(list).get(0).options[$(list).get(0).options.length] = new Option("Select a route...", "0");
-//                 $.each(msg.d, function (index, item) {
-//                     $(list).append($("<option />").val(item.id).text(item.name));
-//                     //$(list).get(0).options[$(list).get(0).options.length] = new Option(item.name, item.id);
-//                 });
-//                 $(list).val('0');
-//                 checkListCookie("route", "MainMobileContent_routeList");
-//             },
-//             error: function () {
-//                 $("#MainMobileContent_routeList").text("Failed to load routes");
-//             },
-//             complete: function (jqXHR, textStatus) {
-//                 $("#routeWait").addClass("hidden");
-//             }
-//         });
-//         $("span").remove();
-//         $(".dropList").select2();
-//     }
-
-//     function getDirections() {
-//         reset();
-
-//         // Clear cookies if	this is	a new selection
-//         if (!initialView) {
-//             $.cookie("direction", null);
-//             $.cookie("stop", null);
-//         }
-
-//         if (settings.includeStops) {
-//             $("#MainMobileContent_stopList").get(0).options.length = 0;
-//         }
-
-
-//         var list = $("#MainMobileContent_directionList");
-//         $(list).empty();
-//         $("#MainMobileContent_stopList").empty();
-//         $(list).get(0).options.length = 0;
-//         //$("#MainMobileContent_directionList").text("Loading	directions...");
-//         $("#directionWait").removeClass("hidden");
-
-//         $.ajax({
-//             type: "POST",
-//             url: "http://webwatch.rtcwashoe.com/TMwebwatch/Arrivals.aspx/getDirections",
-//             data: "{routeID: " + $("#MainMobileContent_routeList").val() + "}",
-//             contentType: "application/json;	charset=utf-8",
-//             dataType: "json",
-//             success: function (msg) {
-//                 if (msg.d == null || msg.d.length == 0) {
-//                     $("#MainMobileContent_directionList").text("No directions found");
-//                     return;
-//                 }
-
-//                 $(list).get(0).options[$(list).get(0).options.length] = new Option("Select a direction...", "");
-//                 $.each(msg.d, function (index, item) {
-//                     $(list).append($("<option />").val(item.id).text(item.name));
-//                     //$(list).get(0).options[$(list).get(0).options.length] = new Option(item.name, item.id);
-//                 });
-
-//                 checkListCookie("direction", "MainMobileContent_directionList");
-
-//                 if (!settings.includeStops)
-//                     initialView = false;
-//             },
-//             error: function () {
-//                 $("#MainMobileContent_directionList").text("Failed to load directions");
-//             },
-//             complete: function (jqXHR, textStatus) {
-//                 $("#directionWait").addClass("hidden");
-//             }
-//         });
-//         $("span").remove();
-//         $(".dropList").select2();
-//     }
-
-//     function getStops() {
-//         // Clear cookies if	this is	a new selection
-//         if (!initialView)
-//             $.cookie("stop", null);
-
-//         var list = $("#MainMobileContent_stopList");
-
-//         $(list).get(0).options.length = 0;
-//         //$("#MainMobileContent_stopList").text("Loading stops...");
-//         $("#stopWait").removeClass("hidden");
-
-//         $.ajax({
-//             type: "POST",
-//             url: "http://webwatch.rtcwashoe.com/TMwebwatch/Arrivals.aspx/getStops",
-//             data: "{routeID: " + $("#MainMobileContent_routeList").val() + ",	directionID: " + $("#MainMobileContent_directionList").val() + "}",
-//             contentType: "application/json;	charset=utf-8",
-//             dataType: "json",
-//             success: function (msg) {
-//                 if (msg.d == null || msg.d.length == 0) {
-//                     $("#MainMobileContent_stopList").text("No stops	found");
-//                     return;
-//                 }
-//                 $(list).empty();
-//                 $(list).get(0).options[$(list).get(0).options.length] = new Option("Select a stop...", "");
-
-//                 $.each(msg.d, function (index, item) {
-//                     $(list).append($("<option />").val(item.id).text(item.name));
-//                     //$(list).get(0).options[$(list).get(0).options.length] = new Option(item.name, item.id);
-//                 });
-
-//                 checkListCookie("stop", "MainMobileContent_stopList");
-
-//                 initialView = false;
-//             },
-//             error: function () {
-//                 $("#MainMobileContent_stopList").text("Failed to load stops");
-//             },
-//             complete: function (jqXHR, textStatus) {
-//                 $("#stopWait").addClass("hidden");
-//             }
-//         });
-//         $("span").remove();
-//         $(".dropList").select2();
-//     }
-
-//     function getArrivalTimes(refresh) {
-//         if (!refresh) {
-//             reset(true);
-//             $("#stopWait").removeClass("hidden");
-//         }
-
-//         $.ajax({
-//             type: "POST",
-//             url: "http://webwatch.rtcwashoe.com/TMwebwatch/Arrivals.aspx/getStopTimes",
-//             data: "{routeID: " + $("#MainMobileContent_routeList").val() + ",	directionID: " + $("#MainMobileContent_directionList").val() + ",	stopID:	" + $("#MainMobileContent_stopList").val() + ", useArrivalTimes:	" + settings.arrivals + "}",
-//             contentType: "application/json;	charset=utf-8",
-//             dataType: "json",
-//             success: function (msg) {
-//                 if (msg.d == null) {
-//                     msg.d = { errorMessage: "Sorry, an internal error has occurred" };
-//                 }
-
-// 				if (msg.d.errorMessage == null && (msg.d.routeStops == null || msg.d.routeStops[0].stops == null || msg.d.routeStops[0].stops[0].crossings == null || msg.d.routeStops[0].stops[0].crossings.length == 0))
-// 					msg.d.errorMessage = "No upcoming stop times found";
-
-// 				if (msg.d.errorMessage != null)
-// 				{
-// 					displayError(msg.d.errorMessage);
-// 					return;
-// 				}
-
-// 				msg.d.stops = msg.d.routeStops[0].stops;
-
-// 				var count = msg.d.stops[0].crossings.length;
-// 				msg.d.heading = "Next " + (count > 1 ? count : "") + " Vehicle " + settings.headingLabel + (count > 1 ? "s" : "");
-
-//                 var result = $("#stopTemplate").render(msg.d);
-
-//                 if (refresh)
-//                     $("#resultBox").html($(result).html());
-//                 else
-//                     displayResultsBox(result);
-
-//                 if (!refresh)
-//                     timer = window.setInterval(function () {
-//                         getArrivalTimes(true);
-//                     }, 30000);
-//             },
-//             error: function () {
-//                 displayError("Failed to	load stop times");
-//             },
-//             complete: function (jqXHR, textStatus) {
-//                 $("#stopWait").addClass("hidden");
-//             }
-//         });
-//         $("span").remove();
-//         $(".dropList").select2();
-//     }
-
-//     function displayError(error) {
-//         reset(true);
-//         displayResultsBox($("#errorTemplate").render({ error: error }));
-//     }
-
-//     function displayResultsBox(html) {
-//         // Unfortunately IE9 leaves	artifacts
-//         var radius = $("#contentBox").css("border-radius");
-
-//         $(html).hide().appendTo("#contentBox").toggle(500, function () {
-//             $("#contentBox").css("border-radius", radius);
-//             $(this).animate({ opacity: "1" }, 200);
-//         });
-//     }
-
-//     function reset(instantRemove) {
-//         if (timer != null) {
-//             window.clearInterval(timer);
-//             timer = null;
-//         }
-
-//         if ($("#resultBox").length > 0) {
-//             if (instantRemove)
-//                 $("#resultBox").remove();
-//             else
-//                 removeResultBox();
-//         }
-//     }
-
-//     function removeResultBox() {
-//         // Unfortunately IE9 leaves	artifacts
-//         var shadow = $("#contentBox").css("box-shadow");
-//         var shadowHide = shadow;
-
-//         $("#resultBox").animate({ opacity: "0" }, 200, function () {
-//             $("#contentBox").css("box-shadow", shadowHide);
-//             $(this).toggle(500, function () {
-//                 $("#contentBox").css("box-shadow", shadow);
-//                 $(this).remove();
-//             })
-//         });
-//     }
-
-//     return {
-//         displayError: displayError
-//     };
-// }
+document.getElementById("screen").style.display = 'none'; 
+}
